bbd974361bd4be97b8f606de79b13512ce9849fc
Will Poynter <will@researchwiseai.com>
2025-06-12 19:30:59 +0100

    Improved the build system

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    minify: false,
+    treeshake: true,
+    outDir: 'dist',
+    tsconfig: 'tsconfig.build.json',
+    define: {
+        'import.meta.vitest': 'undefined',
+        'process.env.DEBUG_PROXY': 'false', // Disable the debug proxy in the build
+    },
+    external: ['undici'],
+})
